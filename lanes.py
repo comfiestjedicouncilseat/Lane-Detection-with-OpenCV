1a95544edab1c6e361d92c88696e381e9bd49254
Michael Tao <michaeltao0713@gmail.com>
2023-11-29 11:08:32 -0500

    Gray, blur and canny an image

--- lanes.py
+++ lanes.py
@@ -11,14 +11,15 @@ lane_img = np.copy(img)
 
 # Gray scale the image for single channel calculations
 gray = cv.cvtColor(lane_img, cv.COLOR_BGR2GRAY)
-cv.imshow("Gray Road", gray)
 
-# Blur to reduce amount of edges detected
-# blur = cv.GaussianBlur(gray, (5, 5), cv.BORDER_DEFAULT)
-# cv.imshow("Blur", blur)
+# Blur to reduce amount of edges detected, reduce image noise
+# Kernel size of 5 by 5
+# Deviation of 0
+blur = cv.GaussianBlur(gray, (5, 5), 0)
+cv.imshow("Blur", blur)
 
 # Run canny edge detection on the image
-canny = cv.Canny(gray, 125, 175)
+canny = cv.Canny(blur, 125, 175)
 cv.imshow("Edges Detected", canny)
 
 
